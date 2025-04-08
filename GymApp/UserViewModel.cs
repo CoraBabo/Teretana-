416b1ce495ec6a001b4f777661f214c87759bd7e
CoraBabo <hamza.krkalic.22@size.ba>
2025-04-09 01:08:36 +0200

    prvi commit

---
+++ GymApp/UserViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using GymApp;
+namespace GymApp;
+public class UserViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<User> Users { get; set; } = new ObservableCollection<User>();
+
+    private string firstName;
+    private string lastName;
+    private string email;
+
+    public string FirstName
+    {
+        get => firstName;
+        set { firstName = value; OnPropertyChanged(); }
+    }
+
+    public string LastName
+    {
+        get => lastName;
+        set { lastName = value; OnPropertyChanged(); }
+    }
+
+    public string Email
+    {
+        get => email;
+        set { email = value; OnPropertyChanged(); }
+    }
+
+    public Command AddUserCommand { get; }
+
+    public UserViewModel()
+    {
+        AddUserCommand = new Command(AddUser);
+    }
+
+    private void AddUser()
+    {
+        if (!string.IsNullOrWhiteSpace(FirstName) && !string.IsNullOrWhiteSpace(LastName))
+        {
+            Users.Add(new User { FirstName = FirstName, LastName = LastName, Email = Email });
+            FirstName = LastName = Email = string.Empty;
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    void OnPropertyChanged([CallerMemberName] string name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
